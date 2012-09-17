a09734cdab2a057900f072ad4f03da7337fba1af
Tuomas Jorma Juhani Räsänen <tuomasjjrasanen@tjjr.fi>
2012-09-17 18:12:41 +0300

    lsusb_bomb.sh is now dash compatible

--- lsusb_bomb.sh
+++ lsusb_bomb.sh
@@ -1,15 +1,15 @@
-#!/bin/bash
+#!/bin/sh
 
 set -eu
 
 lsusb_loop() {
-    for i in {1..10000}
+    for i in $(seq 1 10000)
     do
 	lsusb -v >/dev/null 2>&1
     done
 }
 
-for i in {1..15}
+for i in $(seq 1 15)
 do
     lsusb_loop&
 done
