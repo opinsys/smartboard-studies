cb3289ea487222b67aafb32f2e3fc9c9989dc964
Tuomas Jorma Juhani Räsänen <tuomasjjrasanen@tjjr.fi>
2012-09-17 18:30:23 +0300

    lsusb_bomb: bind sequence number to "anonymous" variable _

--- lsusb_bomb.sh
+++ lsusb_bomb.sh
@@ -3,13 +3,13 @@
 set -eu
 
 lsusb_loop() {
-    for i in $(seq 1 10000)
+    for _ in $(seq 1 10000)
     do
 	lsusb -v >/dev/null 2>&1
     done
 }
 
-for i in $(seq 1 15)
+for _ in $(seq 1 15)
 do
     lsusb_loop&
 done
