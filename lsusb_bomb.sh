325f05e0aa84d8610bebb0cabbc5b8a6d4dbe913
Tuomas Jorma Juhani Räsänen <tuomasjjrasanen@opinsys.fi>
2012-09-17 10:54:53 +0300

    Run more lsusbs and run them longer

--- lsusb_bomb.sh
+++ lsusb_bomb.sh
@@ -3,13 +3,13 @@
 set -eu
 
 lsusb_loop() {
-    for i in {1..1000}
+    for i in {1..10000}
     do
 	lsusb -v >/dev/null 2>&1
     done
 }
 
-for i in {1..10}
+for i in {1..15}
 do
     lsusb_loop&
 done
